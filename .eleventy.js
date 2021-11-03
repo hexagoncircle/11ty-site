3ebed65390f58c94380b0813750aabf889c836a1
Ryan Mulligan <rmulligan@inspirato.com>
2021-11-03 13:21:01 -0700

    Added randomPost filter and updated index content

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,13 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
   eleventyConfig.addPassthroughCopy("./src/css");
 
+  eleventyConfig.addFilter("randomPost", (arr) => {
+    arr.sort(() => {
+      return 0.5 - Math.random();
+    });
+    return arr.slice(0, 1);
+  });
+
   return {
     dir: {
       input: "src",
--- src/index.md
+++ src/index.md
@@ -1,6 +1,13 @@
 ---
 title: "Hello World"
 layout: "base.njk"
+templateEngineOverride: njk,md
 ---
 
 This is a homepage.
+
+## Random post
+
+{% for post in collections.posts | randomPost %}
+<a href="{{ post.url }}">{{ post.data.title }}</a>
+{% endfor %}
